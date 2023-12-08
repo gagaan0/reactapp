ea4b24a6ac6cf870c0219541c47d4f91f4cc6572
gagaan0 <143601990+gagaan0@users.noreply.github.com>
2023-12-08 21:27:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const myElement=<h1>welcome to jsx!</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
-
-
-
-const x=6;
-let text="odd";
-if(l>5){
-  m="yes";
-}
-const k=<h1>{m}</h1>
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(k);
-
-
-
-const x=6;
-let text="odd";
-if(x%2===0){
-  text="even";
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(text);
-
-
-class Car extends React.Component{
-render(){
-return <h1>hi, welcome!</h1>;
-}
-}
-const container=document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<Car/>);
-
-
-
-function Car(){
-  return <h1>hi,hello</h1>
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car/>);
-
-
 function Car(props){
   return <h2>i am a {props.color} car!</h2>;
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Car color="red"/>);
-
 
 function Car(props){
   return <h1>this is a car!</h1>;
@@ -67,46 +21,12 @@ function Garage(){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Garage/>);
 
-
-import Car from './car.js';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car/>);
-
-
-class Car extends React.Component{
-  render(){
-    return <h1>welcome to class</h1> 
-  }
-}
-const container=document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<Car/>);
+            
+            
+--------------------------------
 
 
-
-class A extends React.Component{
-  render(){
-    return <h1>welcome to {this.props.color} class</h1>
-  }
-}
-const container=document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<A color="property"/>);
-
-
-class Car extends React.Component{
-  constructor(props)
-  {
-    super(props);
-  }
-  render(){
-    return <h2>i am a {this.props.model}!</h2>
-  }
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car model='audi'/>);
-
-
+  
 class Car extends React.Component{
   constructor(props)
   {
@@ -132,114 +52,10 @@ const root = ReactDOM.createRoot(container);
 root.render(<Car/>);
 
 
-
-class Car extends React.Component{
-  constructor(props)
-  {
-    super(props);
-    this.state={
-      brand:"ford",
-      model:"mustang",
-      color:"red",
-      year:1969,
-    };
-  }
-  changeColor=()=>{
-    this.setState({color:"blue"});
-  }
-  render(){
-    return(
-      <div>
-    <h1>my {this.state.brand}</h1>
-    <p>it is a {this.state.color} {this.state.model} from {this.state.year} </p>
-    <button type="button" onClick={this.changeColor}>changeColor</button>
-    </div>
-    );
-  }
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car/>);
-
-
-class Car extends React.Component{
-  constructor(props)
-  {
-    super(props);
-    this.state={favoritecolor:"black"};
-  }
-  static getDerivedStateFromProps(props,state){
-    return {favoritecolor:props.favcol};
-  }
-  render(){
-    return <h1>my favorite color is {this.state.favoritecolor}</h1>;
-  }
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car favcol="orange"/>);
-
-
-class Header extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={Favoritecolor:"red"};
-  }
-  componentDidMount(){
-    setTimeout(()=>{this.setState({Favoritecolor:"yellow"})},1000)
-  }
-  render(){
-    return(<h1>my Fovorite color is{this.state.Favoritecolor} </h1>);
-  }
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Header/>);
-
-
-
-function Football(){
-  const shot=()=>{
-    alert("great shot!");
-  }
-  return(
-    <button onClick={shot}>take the shot !</button>
-  );
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football/>);
-
-
-
-function MissedGoal(){
-  return  <h1>MISSED!</h1>;
-}
-function MadeGoal(){
-  return  <h1>Goal!</h1>;
-}
-function Goal(props){
-  const isGoal=props.isGoal;
-  if(isGoal){
-    return <MadeGoal/>;
-  }
-  return <MissedGoal/>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Goal isGoal={false}/>);
-
-
-
-function Football(){
-  const shoot=(a)=>{
-    alert(a);
-  }
-  return(
-    <button onClick={()=>shoot("Goal!")}>take a shoot</button>
-  );
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football/>);
-
 <----------------------------------------------------------------------------------------------------> 
 
+
+            
 import { useState } from "react";
 function MyForm(){
   const[inputs,setInputs]=useState({});
@@ -277,6 +93,8 @@ function MyForm(){
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(<MyForm/>);
 
+
+           
 import { useEffect, useState } from "react";
 function Counter(){
   const[count,setCount]=useState(0);
@@ -294,6 +112,8 @@ function Counter(){
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(<Counter/>);
 
+              
+              
   import { createContext, useContext, useState } from "react";
   const userContext =createContext();
   function Component1() {
@@ -340,85 +160,6 @@ function Counter(){
   }
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(<Component1 />);
-
-
-  
-import { useEffect, useRef, useState } from "react";
-  function Apple(){
-    const[inputValue,setInputValue]=useState("");
-    const previousInputValue=useRef("");
-    useEffect(()=>{
-      previousInputValue.current=inputValue;
-    },[inputValue]);
-    return(
-      <>
-      <input type="text"
-      value={inputValue}
-      onChange={(e)=>setInputValue(e.target.value)}
-      />
-      <h2>current value:{inputValue}</h2>
-      <h2>previous value:{previousInputValue.current}</h2>
-      </>
-    );
-  }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Apple/>);
-
-
-
-import { useReducer } from "react";
-const initialTodos=[
-  {
-    id:1,
-    title:"todo 1",
-    complete:false,
-  },
-  {
-    id:2,
-    title:"todo 2",
-    complete:false,
-  },
-];
-const reducer=(state,action)=>{
-  switch(action.type){
-    case "COMPLETE":
-      return state.map((todo)=>{
-        if(todo.id===action.id){
-          return{...todo,complete:!todo.complete};
-        }else{
-          return todo;
-        }
-      });
-      default:
-        return state;
-  }
-};
-function Todos(){
-  const[todos,dispatch]=useReducer(reducer,initialTodos);
-  const handleComplete=(todo)=>{
-    dispatch({type:"COMPLETE",id:todo.id});
-  };
-  return(
-    <>
-    {todos.map((todo)=>(
-      <div key={todo.id}>
-      <label>
-        <input
-        type="checkbox"
-        checked={todo.complete}
-        onChange={()=>handleComplete(todo)}
-        />
-        {todo.title}
-      </label>
-      </div>
-    ))}
-    </>
-  );
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Todos/>);
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
